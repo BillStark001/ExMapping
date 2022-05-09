5756832b63f1dfbbb8d4d1041902d3877a29a8ab
Toshiaki Subashiko <31788509+BillStark001@users.noreply.github.com>
2022-05-09 11:25:16 +0900

    Initial

---
+++ ExMapping.Tests/ParsingTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExMapping;
+using System.Collections.Generic;
+
+namespace ExMapping.Tests
+{
+    using DSS = Dictionary<string, string>;
+
+    [TestClass]
+    public class ParsingTests
+    {
+
+        
+
+        [TestMethod]
+        public void TestTestItself()
+        {
+            Assert.AreNotEqual(114514, 1919810);
+        }
+
+        [TestMethod]
+        public void TestBasicParsing()
+        {
+            var inStr = @"# test
+a=b
+a = b
+multi=
+&line1
+$line2   
+&continue line2
+$
+";
+            var outMap = Utils.Parse(inStr);
+            Assert.AreEqual(3, outMap.Count);
+            Assert.AreEqual(outMap["a"], "b");
+            Assert.AreEqual(outMap["a "], " b");
+            Assert.AreEqual(outMap["multi"], "line1\nline2   continue line2\n");
+        }
+
+        public void TestEscape()
+        {
+            var inStr = @"# test
+a=b
+a = b
+multi=
+&line1
+$line2   
+&continue line2
+$
+";
+            var outMap = Utils.Parse(inStr);
+            Assert.AreEqual(3, outMap.Count);
+            Assert.AreEqual(outMap["a"], "b");
+            Assert.AreEqual(outMap["a "], " b");
+            Assert.AreEqual(outMap["multi"], "line1\nline2   continue line2\n");
+        }
+    }
+}
